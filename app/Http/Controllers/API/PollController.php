63a8b39762b9352e5625fdcdde4e10e8266bbb0c
mikheil <m.abramyan@fortunejack.com>
2019-07-11 16:47:13 +0400

    request

---
+++ app/Http/Controllers/API/PollController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PollController extends Controller
+{
+    //
+    public function getPoll(Request $request)
+    {
+
+    }
+}
+ 
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Http\Request;
 
 Route::post('login', 'API\UserController@login');
 //Route::post('register', 'API\UserController@register');
-Route::group(['middleware' => 'auth:api'], function(){
-Route::post('details', 'API\UserController@details');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('details', 'API\UserController@details');
+    Route::get('poll', 'API\UserController@details');
 });
