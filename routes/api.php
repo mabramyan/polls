669e406b068bb07e532a4c2569980c52f4a714ac
mikheil <m.abramyan@fortunejack.com>
2019-07-29 11:58:57 +0400

    change post to get

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
-Route::post('login', 'API\UserController@login');
+Route::get('login', 'API\UserController@login');
 //Route::post('register', 'API\UserController@register');
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('details', 'API\UserController@details');
