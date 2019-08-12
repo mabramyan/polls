f39c001f6361a8af4cfb2dbb81af2b5437700ffd
Nino Matskepladze <ninomatskepladze825@gmail.com>
2019-08-12 18:22:42 +0400

    migration

---
+++ database/migrations/2019_08_12_141611_update_polls_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class UpdatePollsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('polls')->where(['finished' => 1])->update(['finished' => 0]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
